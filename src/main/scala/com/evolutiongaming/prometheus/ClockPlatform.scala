7dd29e22d6f61589f8527bc9bc6ac017a92419d4
Mikhail Sokolov <mikhail.g.sokolov@gmail.com>
2024-08-30 16:44:16 +0300

    Migrate to nanoTime for time* methods, improve API DX
    timeFunc and timeFuture used millisecond precision, which lead to issues like
    Summary quantiles being skewed towards 0 if measured latencies are close to 1 ms.
    
    To fix that:
    - time* methods use System.nanoTime() now
    - deprecate specific time*Nano methods, millisecond precision shouldn't be a choice to avoid mistakes
    - improve method naming, document all time-related public functionality
    - deprecate unused TemporalOps implicit conversion
    - overall developer experience improvements
    
    This change breaks source compatibility for some usages, but binary compatibility is preserved.

--- src/main/scala/com/evolutiongaming/prometheus/ClockPlatform.scala
+++ src/main/scala/com/evolutiongaming/prometheus/ClockPlatform.scala
@@ -1,14 +1,28 @@
 package com.evolutiongaming.prometheus
 
+/** Exposes platform time measuring capabilities needed for metrics
+  */
 trait ClockPlatform {
+
+  /** @see
+    *   [[System#currentTimeMillis()]]
+    */
   def nowMillis: Long
+
+  /** @see
+    *   [[System#nanoTime()]]
+    */
   def nowNano: Long
 }
 
 object ClockPlatform {
 
+  /** Global singleton instance for default [[ClockPlatform]] for JVM
+    */
   val default: ClockPlatform = new Default
 
+  /** Default [[ClockPlatform]] impl for JVM - use [[ClockPlatform.default]] global singleton instance!
+    */
   class Default extends ClockPlatform {
     override def nowMillis: Long = System.currentTimeMillis()
     override def nowNano: Long   = System.nanoTime()
--- src/test/scala/com/evolutiongaming/prometheus/ObserveDurationSpec.scala
+++ src/test/scala/com/evolutiongaming/prometheus/ObserveDurationSpec.scala
@@ -1,47 +1,129 @@
 package com.evolutiongaming.prometheus
 
-import com.evolutiongaming.concurrent.CurrentThreadExecutionContext
 import io.prometheus.client.Collector
 
-import scala.concurrent.ExecutionContext
+import scala.collection.mutable
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.Success
 
 class ObserveDurationSpec extends munit.FunSuite with munit.Assertions {
 
-  private implicit val ec: ExecutionContext = CurrentThreadExecutionContext
+  private val TenthOfNsInSec: Double = nsToSec(1L) / 10
 
-  def cmp(x: Double, y: Double): Boolean = Math.abs(x - y) <= 0.000000001
+  test("timeTillNowMillis should correctly convert duration into double") {
+    val durMs = 667L
 
-  def testClockPlatform(v: Long) = new ClockPlatform {
-    override def nowMillis: Long = v
-    override def nowNano: Long   = v
+    implicit val hasObserve: TestHasObserve = new TestHasObserve
+    implicit val clock: ClockPlatform       = testConstMillisClock(durMs)
+
+    val observeDuration = ObserveDuration.create(())
+
+    observeDuration.timeTillNowMillis(startMs = 0L)
+
+    hasObserve.verifyObserved(msToSec(durMs))
   }
 
-  def testHasObserver(expected: Double) = new HasObserve[Unit] {
+  test("timeTillNowNanos should correctly convert duration into double") {
+    val durNs = 730598L
 
-    override def observe(observer: Unit, duration: Double): Unit = {
-      assert(cmp(expected, duration), clue = s"Expected duration $expected did not match calculated $duration")
+    implicit val hasObserve: TestHasObserve = new TestHasObserve
+    implicit val clock: ClockPlatform       = testConstNanosClock(durNs)
+
+    val observeDuration = ObserveDuration.create(())
+
+    observeDuration.timeTillNowNanos(startNs = 0L)
+
+    hasObserve.verifyObserved(nsToSec(durNs))
+  }
+
+  test("timeFunc should measure time with nanosecond precision") {
+    val startTimeNs = 12345L
+    val durNs       = 1000L
+
+    implicit val hasObserve: TestHasObserve = new TestHasObserve
+    implicit val clock: TestNanoClock       = new TestNanoClock(initialValue = startTimeNs)
+
+    val impl = ObserveDuration.create(())
+
+    impl.timeFunc {
+      clock.advanceClock(durNs)
+    }
+
+    hasObserve.verifyObserved(nsToSec(durNs))
+  }
+
+  test("timeFuture should measure time with nanosecond precision") {
+    val durNs = 1L
+
+    implicit val hasObserve: TestHasObserve = new TestHasObserve
+    implicit val clock: TestNanoClock       = new TestNanoClock()
+
+    val observeDuration = ObserveDuration.create(())
+
+    val finishPromise = Promise[Unit]()
+
+    val resultF: Future[String] = observeDuration.timeFuture {
+      finishPromise.future.map { _ =>
+        clock.advanceClock(durNs)
+        "result"
+      }(ExecutionContext.parasitic)
+    }
+
+    assert(!resultF.isCompleted)
+    hasObserve.verifyNothingObserved()
+
+    finishPromise.success(())
+
+    assert(resultF.value.contains(Success("result")))
+    hasObserve.verifyObserved(nsToSec(durNs))
+  }
+
+  private def testConstMillisClock(millis: Long): ClockPlatform = new ClockPlatform {
+    override def nowMillis: Long = millis
+
+    override def nowNano: Long = ???
+  }
+
+  private def testConstNanosClock(nanos: Long): ClockPlatform = new ClockPlatform {
+    override def nowMillis: Long = ???
+
+    override def nowNano: Long = nanos
+  }
+
+  private def nsToSec(nanos: Long): Double = nanos.toDouble / Collector.NANOSECONDS_PER_SECOND
+
+  private def msToSec(nanos: Long): Double = nanos.toDouble / Collector.MILLISECONDS_PER_SECOND
+
+  private final class TestHasObserve extends HasObserve[Unit] {
+    private val observed: mutable.ArrayBuffer[Double] = mutable.ArrayBuffer.empty
+
+    override def observe(observer: Unit, value: Double): Unit = {
+      observed += value
+    }
+
+    def verifyObserved(value: Double): Unit = {
+      assert(observed.size == 1, s"only 1 value expected to be observed, got $observed")
+      assertEqualsDouble(
+        obtained = observed.head,
+        expected = value,
+        delta = TenthOfNsInSec
+      )
+    }
+
+    def verifyNothingObserved(): Unit = {
+      assert(observed.isEmpty, s"no values should be observed, got $observed")
     }
   }
 
-  test("TimeToNow should correctly convert duration into double") {
-    val expectedLong   = 667L
-    val expectedDouble = expectedLong.toDouble / Collector.MILLISECONDS_PER_SECOND
+  private final class TestNanoClock(initialValue: Long = 12345L) extends ClockPlatform {
+    private var currentValue: Long = initialValue
 
-    implicit val hasObserve: HasObserve[Unit] = testHasObserver(expectedDouble)
-    implicit val clock: ClockPlatform         = testClockPlatform(expectedLong)
+    override def nowMillis: Long = ???
 
-    val observeDuration = ObserveDuration.fromHasObserver(())
-    observeDuration.timeTillNow(0L)
-  }
+    override def nowNano: Long = currentValue
 
-  test("TimeToNowNano should correctly convert duration into double") {
-    val expectedLong   = 730598L
-    val expectedDouble = expectedLong.toDouble / Collector.NANOSECONDS_PER_SECOND
-
-    implicit val hasObserve: HasObserve[Unit] = testHasObserver(expectedDouble)
-    implicit val clock: ClockPlatform         = testClockPlatform(expectedLong)
-
-    val observeDuration = ObserveDuration.fromHasObserver(())
-    observeDuration.timeTillNowNanos(0L)
+    def advanceClock(nanos: Long): Unit = {
+      currentValue += nanos
+    }
   }
 }
